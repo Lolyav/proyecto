75841ce1e6622ac0e2144dcddc9d8cacfeb328c3
Lolyav <lalemany521@hotmail.com>
2019-10-15 13:13:27 -0300

    Modificaciones en modulos y rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,34 +6,28 @@ import { NgModule } from '@angular/core';
 import { APP_ROUTES } from './app.routes';
 
 
+// modulos
+
+import { PagesModule } from './pages/pages.module';
+
+
+
+
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { NonpagefoundComponent } from './shared/nonpagefound/nonpagefound.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
-import { PagesComponent } from './pages/pages.component';
 import { RegisterComponent } from './login/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    NonpagefoundComponent,
-    DashboardComponent,
-    ProgressComponent,
-    HeaderComponent,
-    SidebarComponent,
-    BreadcrumbsComponent,
-    PagesComponent,
     RegisterComponent
   ],
   imports: [
     BrowserModule,
-    APP_ROUTES
+    APP_ROUTES,
+    PagesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,9 @@ import { RouterModule, Routes } from '@angular/router';
 import { PagesComponent } from './pages/pages.component';
 
 
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { NonpagefoundComponent } from './shared/nonpagefound/nonpagefound.component';
 import { RegisterComponent } from './login/register.component';
+import { NonpagefoundComponent } from './shared/nonpagefound/nonpagefound.component';
 
 
 
@@ -17,13 +15,6 @@ import { RegisterComponent } from './login/register.component';
 
 
 const appRoutes: Routes = [
-        { path: '', component: PagesComponent,
-    children: [
-        { path: 'dashboard', component: DashboardComponent },
-        { path: 'progress', component: ProgressComponent },
-        { path: '', redirectTo: '/dashboard', pathMatch: 'full' }
-
-    ] },
         { path: 'login', component: LoginComponent },
         { path: 'register', component: RegisterComponent },
         { path: '**', component: NonpagefoundComponent }
